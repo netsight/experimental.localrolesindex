70fdf0435a5e2f45749f34df7716d7be72a93d95
Matthew Russell <mattr@netsight.co.uk>
2014-05-02 11:13:50 +0100

    Created methods to ensure nodes exist to a child corresponding to the context. Add method to set security information.

--- src/experimental/localrolesindex/shadowtree.py
+++ src/experimental/localrolesindex/shadowtree.py
@@ -17,12 +17,34 @@ class Node(BTrees.family64.OO.BTree):
         self.id = id
         self.__parent__ = parent
 
-    def add_descendant(self, obj):
-        node = type(self)(parent=self, id=obj.getId())
-        node.physical_path = obj.getPhysicalPath()
-        node.block_inherit_roles = getattr(obj, '__ac_local_roles_block__', False)
-        node.token = hash((tuple(obj.allowedRolesAndUsers),
-                           node.block_inherit_roles, ))
-        assert (node.id == obj.getId())
-        self[node.id] = node
+    @classmethod
+    def ensure_path_to(cls, root, obj):
+        """Retieve the shadow node for corresponding object.
+
+        Ensures that each ancestor upon its path is present in the shadow tree.
+        Only the leaf return will be guarenteed to contain local role information.
+
+        :param cls: Node class
+        :param root: The root node of the shadow tree
+        :param obj: 
+        :returns: 
+        :rtype: 
+
+        """
+        node = root
+        for comp in filter(bool, obj.getPhysicalPath()):
+            if comp not in node:
+                parent = node
+                node = cls(parent=parent, id=comp)
+                parent[node.id] = node
+            else:
+                node = node[comp]
+        node.update_security_info(obj)
         return node
+
+    def update_security_info(self, obj):
+        self.physical_path = obj.getPhysicalPath()
+        self.block_inherit_roles = getattr(obj, '__ac_local_roles_block__', False)
+        self.token = hash((tuple(obj.allowedRolesAndUsers),
+                           self.block_inherit_roles, ))
+        assert (self.id == obj.getId())
--- src/experimental/localrolesindex/tests/test_shadowtree.py
+++ src/experimental/localrolesindex/tests/test_shadowtree.py
@@ -5,30 +5,67 @@ from experimental.localrolesindex.tests.utils import Dummy
 class TestNode(unittest.TestCase):
 
     def setUp(self):
-        self.root = self.make_one('portal')
+        self.root = self.make_one()
+
+    def get_target_class(self):
+        from experimental.localrolesindex.shadowtree import Node
+        return Node
 
     def make_one(self, *args, **kw):
-        from experimental.localrolesindex.shadowtree import Node
+        cls = self.get_target_class()
+        return cls(*args, **kw)
 
-        return Node(*args, **kw)
+    def test_update_security_info(self):
+        node = self.make_one('foobar', parent=self.root)
+        self.assertEqual(node.id, 'foobar')
+        self.assertIs(node.__parent__, self.root)
+        self.assertIsNone(node.token)
+        self.assertIsNone(node.physical_path)
+        self.assertFalse(node.block_inherit_roles)
+        node.update_security_info(Dummy('/foobar', ['Editor'], local_roles_block=True))
+        self.assertEqual(node.id, 'foobar')
+        self.assertIs(node.__parent__, self.root)
+        self.assertIsInstance(node.token, int)
+        self.assertEqual(node.physical_path, ('', 'foobar'))
+        self.assertTrue(node.block_inherit_roles)
 
-    def test_add_descendant_one_deep(self):
+    def test_ensure_path_to_one_deep(self):
         dummy = Dummy('/a', ['Anonymous'])
-        leaf = self.root.add_descendant(dummy)
-        self.assertEqual(leaf.__parent__.id, 'portal')
-        self.assertEqual(self.root['a'].physical_path, dummy.getPhysicalPath())
+        Node = self.get_target_class()
+        leaf = Node.ensure_path_to(self.root, dummy)
+        self.assertIn('a', self.root, list(self.root.keys()))
+        self.assertEqual(self.root['a'].id, leaf.id)
+        self.assertIsNone(leaf.__parent__.__parent__)
+        self.assertEqual(leaf.physical_path, dummy.getPhysicalPath())
         self.assertEqual(leaf.id, 'a')
         self.assertIsInstance(leaf.token, int)
         self.assertFalse(leaf.block_inherit_roles)
 
-    def test_add_descendant_many_deep(self):
+    def test_ensure_path_to_many_deep(self):
         dummy = Dummy('/a/b/c', ['Anonymous'])
-        leaf = self.root.add_descendant(dummy)
+        Node = self.get_target_class()
+        leaf = Node.ensure_path_to(self.root, dummy)
+
+        b = leaf.__parent__
+        self.assertEqual(b.id, 'b')
+        self.assertIsNone(b.physical_path)
+        self.assertIsNone(b.token)
+        self.assertFalse(b.block_inherit_roles)
+
+        a = b.__parent__
+        self.assertEqual(a.id, 'a')
+        self.assertIsNone(a.physical_path)
+        self.assertIsNone(a.token)
+        self.assertFalse(a.block_inherit_roles)
+
         self.assertEqual(leaf.__parent__.id, 'b')
-        self.assertEqual(self.root['a']['b']['c'].physical_path, dummy.getPhysicalPath())
+        self.assertEqual(leaf.physical_path, dummy.getPhysicalPath())
         self.assertEqual(leaf.id, 'c')
         self.assertIsInstance(leaf.token, int)
         self.assertFalse(leaf.block_inherit_roles)
+
+    def test_search(self):
+        self.fail()
 
 
 def test_suite():
